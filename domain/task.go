3c8ea6d53c7db0d2f7294969168dd7f709eb3160
unknown <kike2006.em@gmail.com>
2026-07-05 19:10:38 +0200

    added a way to create standart tasks

--- domain/task.go
+++ domain/task.go
@@ -13,6 +13,14 @@ type StandartTask struct {
 	done        bool
 }
 
+func NewStandartTask(title, description string, done bool) StandartTask {
+	return StandartTask{
+		title:       title,
+		description: description,
+		done:        done,
+	}
+}
+
 func (t StandartTask) ShowDetails() string {
 	return fmt.Sprintf("Title: %s | Description: %s | Done: %s", t.title, t.description, t.done)
 }
